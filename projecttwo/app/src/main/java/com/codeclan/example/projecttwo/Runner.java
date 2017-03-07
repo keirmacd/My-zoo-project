b83abbf8674ea34baf8b93494be082e7dfe173a1
Keir Macdonald <keirmacd@hotmail.co.uk>
2017-03-07 11:13:37 +0000

    A lot of stuff has been added

--- projecttwo/app/src/main/java/com/codeclan/example/projecttwo/Runner.java
+++ projecttwo/app/src/main/java/com/codeclan/example/projecttwo/Runner.java
@@ -6,4 +6,21 @@ package com.codeclan.example.projecttwo;
 
 public class Runner {
 
+    Zoo zoo;
+    Enclosure enclosure;
+    Food food;
+    Animal animal;
+
+
+
+    public static void main() {
+
+        new Zoo() = zoo();
+        new Enclosure() = enclosure();
+        new Food() = food();
+        new Animal() = animal();
+    }
+
 }
+
+//System.out.println(animal.getName() + "the " + animal.getSpecies() + "which is a " + animal.getType() + " pokemon");
